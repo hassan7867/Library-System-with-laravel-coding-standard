ff78ee4ce7bb78e8f82c46d3b577071ecbd7d174
hassan7867 <47236240+hassan7867@users.noreply.github.com>
2020-04-14 19:44:59 +0500

    top nav file mpodified

--- resources/views/template/include/top-nav.blade.php
+++ resources/views/template/include/top-nav.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background: #0288d1 !important;">
-    <a class="navbar-brand" href="#" style="color: white">Library System</a>
+    <a class="navbar-brand" href="{{URL::to('')}}/" style="color: white">Library System</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
